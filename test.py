0458a8fbc2a0c8f77467d35c894a15e79710ee82
MIRA (mamabear2) <mira@mira.org>
2020-05-11 11:30:17 -0700

    test sync

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-
-def test(setPosition):
-    hiPosition = int(setPosition / 65536)
-    lowPosition = setPosition % 65536
-    print(f"set: {setPosition} hiPos: {hiPosition}, lowPos: {lowPosition} check: {setPosition} = {hiPosition * 65536 + lowPosition}")
-    return
-
-test(3500)
+test
